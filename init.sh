ae3c66423cda3ce5bd9b3017bb31d1740871eb24
javierIA <javier.flores@ia.center>
2023-02-09 16:15:34 -0700

    la luna prensentia

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
-sudo docker run --interactive --tty --rm --volume=$(pwd)/neo4j/data:/data  --volume=$(pwd)/neo4j/backups:/backups  neo4j/neo4j-admin:5.4.0 neo4j-admin database load neo4j --from-path=/backups --overwrite-destination
+docker run --interactive --tty --rm --volume=$(pwd)/neo4j/data:/data  --volume=$(pwd)/neo4j/backups:/backups  neo4j/neo4j-admin:5.4.0 neo4j-admin database load neo4j --from-path=/backups --overwrite-destination
 
